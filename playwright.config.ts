2999138a7bd73a7c65a7dcbc1a11d42b0b11807f
Christopher Timario <christopher.timario@accenture.com>
2026-03-25 16:34:22 +0800

    Fix CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     trace: "on-first-retry",
   },
   webServer: {
-    command: "just run",
+    command: "npm run dev",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: true,
     timeout: 120000,
